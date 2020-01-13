dac018c51154a9dcb71394e0de57e8f8b626b700
Shahin <meshahin007@gmail.com>
2020-01-13 16:07:04 +0600

    ES6 const

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-alert('Hello World');
+//console.log('Hello World');
+
+
+
+// let age = 32;
+// let low = 30;
+
+// if (age == low) {
+//     console.log("Got a married");
+// } else {
+//     console.log("He's single");
+// }
+
+const age = "26";
+
+console.log(`My age is ${age}`);
