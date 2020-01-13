1f1169aca193b7c2262dcaccf77896172bb73b33
Shahin <meshahin007@gmail.com>
2020-01-13 16:39:26 +0600

    Array

--- index.js
+++ index.js
@@ -4,13 +4,30 @@
 
 // let age = 32;
 // let low = 30;
+// const age = "26";
+// console.log(`My age is ${age}`);
 
-// if (age == low) {
-//     console.log("Got a married");
-// } else {
-//     console.log("He's single");
-// }
 
-const age = "26";
 
-console.log(`My age is ${age}`);
+//Array Start
+const fruit = [
+    'Apple',
+    'Orange'
+];
+
+// new added value
+fruit[3] = 'Malta',
+
+    // Push new value
+    fruit.push('mango');
+
+// Unshift Always array value top 
+fruit.unshift('pears');
+
+// use pop for last one hide
+fruit.pop();
+
+// Output value
+console.log(fruit);
+
+//Array End
